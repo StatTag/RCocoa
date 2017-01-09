fd029f44532eb4352897233d188cb16949cd602f
Luke Rasmussen <luke.rasmussen@gmail.com>
2017-01-09 16:06:18 -0600

    Initial commit

---
+++ RAutomation/RAutomation.h
@@ -0,0 +1,19 @@
+//
+//  RAutomation.h
+//  RAutomation
+//
+//  Created by Luke Rasmussen on 1/5/17.
+//  Copyright © 2017 Luke Rasmussen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RAutomation.
+FOUNDATION_EXPORT double RAutomationVersionNumber;
+
+//! Project version string for RAutomation.
+FOUNDATION_EXPORT const unsigned char RAutomationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RAutomation/PublicHeader.h>
+
+
